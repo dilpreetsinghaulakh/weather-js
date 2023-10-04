2f49ced87a57e76cab0666d1db8d2aecd2590baf
Dilpreet Singh <dilpreetsingh@192.168.1.3>
2023-10-04 18:40:22 +0530

    Added a few more backgrounds

--- src/components/background.js
+++ src/components/background.js
@@ -7,11 +7,15 @@ export default function background(condition) {
 
   const image = document.createElement("img");
   image.className = "hero-image";
-  image.src = "https://unsplash.com/photos/ROVBDer29PQ/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjk2MzUzODkxfA&force=true"; // Clouds
+  image.src = "https://unsplash.com/photos/ROVBDer29PQ/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjk2MzUzODkxfAe"; // Clouds
 
   // image.src ="https://unsplash.com/photos/kCWKTI7OLQg/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NDN8fGNsZWFyJTIwc2t5fGVufDB8fHx8MTY5NjM0NjE0NXww"; // Clear Landscape
 
-  // image.src ="https://unsplash.com/photos/E0e1NmVclRo/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTl8fGZvZ3xlbnwwfHx8fDE2OTYzOTIxODF8MA&force=true"; // Fog
+  // image.src ="https://unsplash.com/photos/E0e1NmVclRo/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTl8fGZvZ3xlbnwwfHx8fDE2OTYzOTIxODF8MA"; // Fog
+
+  // image.src = "https://unsplash.com/photos/qVotvbsuM_c/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8bmlnaHQlMjBza3l8ZW58MHx8fHwxNjk2NDIwMjY5fDA"; // Clear Night Sky
+
+  // image.src = "https://unsplash.com/photos/8RRqtEeqXSU/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTh8fGNsb3VkeSUyMG5pZ2h0JTIwc2t5fGVufDB8fHx8MTY5NjQyNDIzN3ww" // Cloudy Night
 
   background.append(backgroundGradient, image);
 
--- src/components/main.js
+++ src/components/main.js
@@ -4,8 +4,6 @@ import heroSection from "./heroSection";
 export default function mainUi(processedWeather) {
   const content = document.getElementById("content");
 
-  console.log(processedWeather);
-
   const innerContent = document.createElement("div");
   innerContent.className = "inner-content";
 
