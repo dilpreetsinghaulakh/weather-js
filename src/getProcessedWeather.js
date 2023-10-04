aa4c748d362ecefd1a32197b0646a85275b9d061
Dilpreet Singh <dilpreetsingh@192.168.1.3>
2023-10-04 16:16:18 +0530

    Added SI units export data

--- src/getProcessedWeather.js
+++ src/getProcessedWeather.js
@@ -1,5 +1,43 @@
 export default function (siUnitsEnable, weatherData) {
+  let current = {};
+  let days = [];
+
   if (siUnitsEnable) {
-    console.log(weatherData); // Just for testing
+    current = {
+      temp: weatherData.current.temp_c,
+      conditionCode: weatherData.current.condition.code,
+      condition: weatherData.current.condition.text,
+      humidity: weatherData.current.humidity,
+      isDay: weatherData.current.is_day,
+      feelsLike: weatherData.current.feelslike_c,
+      uv: weatherData.current.uv,
+      precipitation: weatherData.current.precip_mm,
+      pressure: weatherData.current.pressure_mb,
+      visibility: weatherData.current.vis_km,
+      wind: weatherData.current.wind_kph,
+      windDirection: weatherData.current.wind_dir,
+      windDegree: weatherData.current.wind_degree,
+    };
+
+    for (let i = 0; i < 3; i++) {
+      const data = weatherData.forecast.forecastday[i].day;
+      days.push({
+        avgTemp: data.avgtemp_c,
+        minTemp: data.mintemp_c,
+        maxTemp: data.maxtemp_c,
+        condition: data.condition.text,
+        conditionCode: data.condition.code,
+      });
+    }
   }
+
+  let location = {
+    name: weatherData.location.name,
+    region: weatherData.location.region,
+    country: weatherData.location.country,
+  };
+
+  return { current, days, location };
 }
+
+// For now hours forecast is not being used
