80a6cf2ebfce160f9abeafb5f022987b57e3ec47
Dilpreet Singh <dilpreetsingh@192.168.1.3>
2023-10-04 17:23:07 +0530

    Added hero section with styles

---
+++ src/components/heroSection.js
@@ -0,0 +1,24 @@
+export default function heroSection(data) {
+  const container = document.createElement("div");
+  container.className = "hero-container";
+
+  const city = document.createElement("p");
+  city.textContent = data.city;
+  city.className = "hero-row1-city";
+
+  const temp = document.createElement("p");
+  temp.textContent = data.temp + "°";
+  temp.className = "hero-temp";
+
+  const condition = document.createElement("p");
+  condition.innerText = data.condition;
+  condition.className = "hero-condition";
+
+  const lastRow = document.createElement("p");
+  lastRow.textContent = `Hi ${data.hi} | Low ${data.low}`;
+  lastRow.className = "hero-last-row"
+
+  container.append(city, temp, condition, lastRow);
+
+  return container;
+}
--- src/components/main.js
+++ src/components/main.js
@@ -9,5 +9,15 @@ export default function mainUi(processedWeather) {
   const innerContent = document.createElement("div");
   innerContent.className = "inner-content";
 
+  innerContent.append(
+    heroSection({
+      city: processedWeather.location.name,
+      temp: processedWeather.current.temp,
+      condition: processedWeather.current.condition,
+      hi: Math.round(processedWeather.days[1].maxTemp),
+      low: Math.round(processedWeather.days[1].minTemp),
+    })
+  );
+
   content.append(background(), innerContent);
 }
