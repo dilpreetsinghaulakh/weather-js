637318bed172df9d030cb8152e29c41f1993e5cf
Dilpreet Singh <dilpreetsingh@192.168.1.7>
2023-10-07 16:24:34 +0530

    Made overlay.js and moved overlay part of search in it.

---
+++ src/components/overlay.js
@@ -0,0 +1,40 @@
+export default function overlay(color, ...args) {
+  const preBgValue =
+    document.documentElement.style.getPropertyValue("--bg-gradient-end");
+
+  document.documentElement.style.setProperty("--bg-gradient-end", color);
+
+  const container = document.createElement("div");
+  container.className = "search-container";
+
+  const closeContainer = () => {
+    container.style.opacity = 0;
+    document.querySelector("html").classList.remove("stop-scroll");
+    setTimeout(() => {
+      container.remove();
+    }, 750);
+  };
+
+  setTimeout(() => {
+    container.style.background = color;
+  }, 1);
+
+  const closeButton = document.createElement("div");
+  closeButton.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`;
+  closeButton.className = "search-close-button";
+
+  closeButton.addEventListener("click", () => {
+    closeContainer();
+    document.documentElement.style.setProperty("--bg-gradient-end", preBgValue);
+  });
+
+  args.forEach((ele) => {
+    container.appendChild(ele);
+  });
+
+  container.append(closeButton);
+
+  document.body.appendChild(container);
+}
--- src/components/search.js
+++ src/components/search.js
@@ -1,34 +1,9 @@
 import getProcessedWeather from "../getProcessedWeather";
 import getWeather from "../getWeather";
 import mainUi from "./main";
+import overlay from "./overlay";
 
 export default function search(apiKey, siUnitsEnable) {
-  const preBgValue =
-    document.documentElement.style.getPropertyValue("--bg-gradient-end");
-
-  document.documentElement.style.setProperty("--bg-gradient-end", "#050505");
-
-  const container = document.createElement("div");
-  container.className = "search-container";
-
-  const closeContainer = () => {
-    container.style.opacity = 0;
-    document.querySelector("html").classList.remove("stop-scroll");
-    setTimeout(() => {
-      container.remove();
-    }, 750);
-  };
-
-  setTimeout(() => {
-    container.style.background = "#050505";
-  }, 1);
-
-  const closeButton = document.createElement("div");
-  closeButton.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-  </svg>`;
-  closeButton.className = "search-close-button";
-
   const searchBar = document.createElement("div");
   searchBar.className = "searchbar-search";
 
@@ -44,8 +19,6 @@ export default function search(apiKey, siUnitsEnable) {
   searchButton.addEventListener("click", () => {
     getWeather(apiKey, input.value)
       .then((data) => {
-      console.log(data)
-
         mainUi(getProcessedWeather(siUnitsEnable, data), siUnitsEnable);
         closeContainer();
       })
@@ -61,12 +34,5 @@ export default function search(apiKey, siUnitsEnable) {
     "Please enter full name (like Stuttgart), or abbreviation (like NYC), or coordinates by a comma (latitude, longitude) of the location that you want to search.";
   info.className = "info-search";
 
-  closeButton.addEventListener("click", () => {
-    closeContainer();
-    document.documentElement.style.setProperty("--bg-gradient-end", preBgValue);
-  });
-
-  container.append(searchBar, info, closeButton);
-
-  document.body.appendChild(container);
+  overlay("#050505", searchBar, info);
 }
