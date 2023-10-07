dd872852348e4f11120a1c1adfacf8b2a406e4a9
Dilpreet Singh <dilpreetsingh@192.168.1.7>
2023-10-07 23:51:29 +0530

    Code improvements

--- src/components/overlay.js
+++ src/components/overlay.js
@@ -1,3 +1,11 @@
+export function closeOverlay() {
+  document.querySelector(".overlay-container").style.opacity = 0;
+  document.querySelector("html").classList.remove("stop-scroll");
+  setTimeout(() => {
+    document.querySelector(".overlay-container").remove();
+  }, 750);
+}
+
 export default function overlay(color, ...args) {
   const preBgValue =
     document.documentElement.style.getPropertyValue("--bg-gradient-end");
@@ -10,15 +18,7 @@ export default function overlay(color, ...args) {
   scrollTo(0, 0);
 
   const container = document.createElement("div");
-  container.className = "search-container";
-
-  const closeContainer = () => {
-    container.style.opacity = 0;
-    document.querySelector("html").classList.remove("stop-scroll");
-    setTimeout(() => {
-      container.remove();
-    }, 750);
-  };
+  container.className = "overlay-container";
 
   setTimeout(() => {
     container.style.background = color;
@@ -28,10 +28,10 @@ export default function overlay(color, ...args) {
   closeButton.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>`;
-  closeButton.className = "search-close-button";
+  closeButton.className = "overlay-close-button";
 
   closeButton.addEventListener("click", () => {
-    closeContainer();
+    closeOverlay();
     document.documentElement.style.setProperty("--bg-gradient-end", preBgValue);
   });
 
--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,7 @@
 import getProcessedWeather from "../getProcessedWeather";
 import getWeather from "../getWeather";
 import mainUi from "./main";
-import overlay from "./overlay";
+import overlay, { closeOverlay } from "./overlay";
 
 export default function search(apiKey) {
   const searchBar = document.createElement("div");
@@ -16,14 +16,29 @@ export default function search(apiKey) {
   searchButton.textContent = "Search";
   searchButton.className = "search-button-search";
 
+  const loading = document.createElement("p");
+  loading.textContent = "Loading...";
+  loading.className = "loading-search";
+
   searchButton.addEventListener("click", () => {
+    loading.style.height = "auto";
+    loading.style.opacity = "1";
+
     getWeather(apiKey, input.value)
       .then((data) => {
-        mainUi(getProcessedWeather(JSON.parse(localStorage.getItem("siUnitsEnabled")), data));
-        closeContainer();
+        sessionStorage.setItem("data", JSON.stringify(data));
+        mainUi(
+          getProcessedWeather(
+            JSON.parse(localStorage.getItem("siUnitsEnabled")),
+            data
+          )
+        );
+        closeOverlay();
       })
-      .catch((error) => {
-        console.log(error);
+      .catch(() => {
+        input.style.border = "1px solid #ec5634";
+        loading.style.height = "0";
+        loading.style.opacity = "0";
       });
   });
 
@@ -34,5 +49,5 @@ export default function search(apiKey) {
     "Please enter full name (like Stuttgart), or abbreviation (like NYC), or coordinates by a comma (latitude, longitude) of the location that you want to search.";
   info.className = "info-search";
 
-  overlay("#050505", searchBar, info);
+  overlay("#050505", searchBar, loading, info);
 }
